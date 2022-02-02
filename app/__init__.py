2e5f048a3e69c6b88097b528f0dd69f50e9c2b65
Kingsly62 <clinton.makori@student.moringaschool.com>
2022-02-02 15:14:54 +0300

    removed config file and init file

--- app/__init__.py
+++
@@ -1,9 +0,0 @@
-from app.views import app
-from flask import Flask
-from .config import DevConfig
-
-# Initializing application
-app = Flask(__name__)
-
-# Setting up configuration
-app.config.from_object(DevConfig)
